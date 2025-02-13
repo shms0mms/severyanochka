bf24a45c216e0b135bdfd611f3aa0af42b2f42f8
Fesyse <maksimlol.mail.com@mail.ru>
2025-02-13 21:34:20 +0800

    remove unnecessary map controls

--- js/maps.js
+++ js/maps.js
@@ -3,13 +3,22 @@ async function initMap() {
 
   function initMaps() {
     maps.forEach(map => {
-      new ymaps.Map(map.id, {
+      const myMap = new ymaps.Map(map.id, {
         center: [61.66854376888902, 50.836547597778186],
         zoom: 14,
         zoomRange: { min: 12, max: 16 },
-        behaviors: ["zoom", "drag", "pinchZoom", "mouseTilt"],
+        behaviors: ["drag", "pinchZoom", "mouseTilt"],
         balloonDesc: `Республика Коми, ${map.getAttribute("data-tab-content")}`,
       })
+
+      myMap.controls.remove("geolocationControl")
+      myMap.controls.remove("rulerControl")
+      myMap.controls.remove("fullscreenControl")
+      myMap.controls.remove("typeSelector")
+      myMap.controls.remove("trafficControl")
+      myMap.controls.remove("routeButton")
+      myMap.controls.remove("searchControl")
+      myMap.controls.remove("zoomControl")
     })
   }
   ymaps.ready(initMaps)

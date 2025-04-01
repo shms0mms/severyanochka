a898e21b7e84d769bdd6309cc7764863b3d45d9f
shms0mms <shmsmms01@gmail.com>
2025-04-01 21:40:36 +0800

    feat: filters logic (not working input with type range)

--- js/switch.js
+++ js/switch.js
@@ -1,23 +1,24 @@
 export const initializeSwitch = () => {
-  const switches = document.querySelectorAll("[data-switch]")
-  if (switches.length !== 0) {
-    switches.forEach(switchEl => {
-      const switchInput = switchEl.querySelector("[data-switch-input] input")
-      const switchIcon = switchEl.querySelector("[data-switch-icon]")
-      if (switchInput.checked) {
-        switchIcon.classList.add("active")
-      }
-      switchEl.addEventListener("click", e => {
-        let target = e.target
-        if (switchIcon !== undefined && target.closest("[data-switch-icon]")) {
-          if (switchInput.checked) {
-            switchInput.setAttribute("checked", false)
-          } else {
-            switchInput.setAttribute("checked", true)
-          }
-          switchIcon.classList.toggle("active")
-        }
-      })
-    })
-  }
-}
+	const switches = document.querySelectorAll("[data-switch]");
+	if (switches.length !== 0) {
+		switches.forEach((switchEl) => {
+			const switchInput = switchEl.querySelector("[data-switch-input] input");
+			const switchIcon = switchEl.querySelector("[data-switch-icon]");
+			if (switchInput.checked) {
+				switchIcon.classList.add("active");
+			}
+			switchEl.addEventListener("click", (e) => {
+				let target = e.target;
+				if (switchIcon !== undefined && target.closest("[data-switch-icon]")) {
+					if (!switchInput.hasAttribute("checked")) {
+						switchInput.setAttribute("checked", "true");
+					} else {
+						switchInput.removeAttribute("checked");
+					}
+
+					switchIcon.classList.toggle("active");
+				}
+			});
+		});
+	}
+};

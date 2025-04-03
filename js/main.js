c06e02ce4f084576114e169da0ee086de4c730fd
Fesyse <maksimlol.mail.com@mail.ru>
2025-04-03 20:40:36 +0800

    feat: price slider works

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import { initializeSpollers } from "./spoller.js"
 import { initializeTabs } from "./tabs.js"
 import { initializeSwitch } from "./switch.js"
+import { initializeRange } from "./range.js"
 
 // Инициализируем примитивы
 initializeSpollers()
 initializeTabs()
 initializeSwitch()
+initializeRange()
